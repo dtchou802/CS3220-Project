1da129270e224c1e5423be2ca6546ec219ccd1db
dtchou802 <dtchou802@gmail.com>
2018-08-01 22:41:15 -0700

    Add files via upload

---
+++ Upload.java
@@ -0,0 +1,66 @@
+package model;
+
+public class Upload {
+	String fileName;
+	String filePath;
+	String action;
+	int id;
+	
+	
+public Upload(String fileName, String filePath) {
+	this.fileName = fileName;
+	this.filePath = filePath;
+	this.action = action;
+	this.id = id;
+}
+
+public Upload(int id, String fileName, String filePath) {
+this.fileName = fileName;
+this.filePath = filePath;
+this.action = action;
+this.id = id;
+}
+
+
+
+public int getId() {
+	return id;
+}
+
+
+public void setId(int id) {
+	this.id = id;
+}
+
+
+public String getFileName() {
+	return fileName;
+}
+
+
+public void setFileName(String fileName) {
+	this.fileName = fileName;
+}
+
+
+public String getFilePath() {
+	return filePath;
+}
+
+
+public void setFilePath(String filePath) {
+	this.filePath = filePath;
+}
+
+
+public String getAction() {
+	return action;
+}
+
+
+public void setAction(String action) {
+	this.action = action;
+}
+
+
+}
