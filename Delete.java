217e23638872fa96fdfd69f6f5de1a36c9dfedf8
ajetty <22653345+ajetty@users.noreply.github.com>
2018-08-04 04:08:56 -0700

    Add files via upload

---
+++ Delete.java
@@ -0,0 +1,108 @@
+package groupFinal;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Upload;
+
+@WebServlet("/Delete")
+public class Delete extends HttpServlet {
+	 private static final long serialVersionUID = 1L;
+
+	 private Upload getFile(Integer id) throws ServletException {
+		 
+	    Upload file = null;
+	    Connection c = null;
+	    
+	    try
+	    {
+	        String url = "jdbc:mysql://cs3.calstatela.edu:3306/cs3220stu06?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	        String username = "cs3220stu06";
+	        String password = "****";
+	
+	        String sql = "select * from guestbook where id = ?";
+	
+	        c = DriverManager.getConnection(url, username, password);
+	        PreparedStatement pstmt = c.prepareStatement(sql);
+	        pstmt.setInt(1, id);
+	        ResultSet rs = pstmt.executeQuery();
+	
+	        if(rs.next())
+	            file = new Upload(rs.getInt("id"), rs.getString("fileName"), rs.getString("filePath"));
+	    }
+	    catch(SQLException e)
+	    {
+	        throw new ServletException(e);
+	    }
+	    finally
+	    {
+	        try
+	        {
+	            if(c != null) c.close();
+	        }
+	        catch(SQLException e)
+	        {
+	            throw new ServletException(e);
+	        }
+	    }
+	
+	    return file;
+	}
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Integer id = Integer.valueOf(request.getParameter("id"));
+        Upload file = getFile(id);
+        String fileName = file.getFileName();
+        String filePath = file.getFilePath();
+
+        Connection c = null;
+        
+        try
+        {
+            String url = "jdbc:mysql://cs3.calstatela.edu:3306/cs3220stu06?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            String username = "cs3220stu06";
+            String password = "****";
+
+            String sql = "delete from upload where id =" + id;
+
+            c = DriverManager.getConnection(url, username, password);
+            
+            PreparedStatement pstmt = c.prepareStatement(sql);
+            
+            pstmt.executeUpdate();
+            
+        }
+        catch(SQLException e)
+        {
+            throw new ServletException(e);
+        }
+        finally
+        {
+            try
+            {
+                if(c != null) c.close();
+            }
+            catch(SQLException e)
+            {
+                throw new ServletException(e);
+            }
+        }
+
+        response.sendRedirect("/databases/FileManager");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	doGet(request, response);
+    }
+
+}
