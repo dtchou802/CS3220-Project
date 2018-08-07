88f7d4349751068734e1d2d8e9dbad5dd53b2084
ajetty <22653345+ajetty@users.noreply.github.com>
2018-08-06 20:16:25 -0700

    Update Download.java

--- Download.java
+++ Download.java
@@ -1,7 +1,9 @@
-package groupFinal;
+package dbs;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -16,7 +18,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Download")
+@WebServlet(urlPatterns = "/databases/Download", loadOnStartup = 1)
 public class Download extends HttpServlet {
 	 private static final long serialVersionUID = 1L;
 	 
@@ -33,13 +35,13 @@ public class Download extends HttpServlet {
 	    
 	    try
 	    {
-	        String url = "jdbc:mysql://cs3.calstatela.edu:3307/cs3220stu06?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-	        String username = "cs3220stu07";
-	        String password = "";
+	        String url = "jdbc:mysql://cs3.calstatela.edu:3306/cs3220stu12?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	        String username = "cs3220stu12";
+	        String password = "EOZ6mrUV";
 	
 	        c = DriverManager.getConnection(url, username, password);
 	        Statement stmt = c.createStatement();
-	        ResultSet rs = stmt.executeQuery("SELECT * FROM tablename WHERE id='" + id + "'");
+	        ResultSet rs = stmt.executeQuery("SELECT * FROM uploads WHERE id='" + id + "'");
 	        
 	        while (rs.next()) {
 	        	filePath = rs.getString("filePath");
@@ -67,27 +69,50 @@ public class Download extends HttpServlet {
 	
 	    return filePath;
 	}
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	 
+	 @Override
+	 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Integer id = Integer.valueOf(request.getParameter("id"));
         String path = getFilePath(id);
+        String fileName = "upload.txt";
+    	
+        response.setContentType("text/plain");
+        response.setHeader("Content-disposition", "attachment; filename=" + fileName);
+ 
+        try(InputStream in = request.getServletContext().getResourceAsStream(path);
+          
+        	OutputStream out = response.getOutputStream()) {
+ 
+        	byte[] buffer = new byte[2048];
+         
+            int numBytesRead;
+            while ((numBytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, numBytesRead);
+            }
+            
+        }
+	 }
 
-        FileInputStream in = new FileInputStream(path);
-        OutputStream out = response.getOutputStream();
-
-        byte buffer[] = new byte[2048];
-        int bytesRead;
-        while( (bytesRead = in.read( buffer )) > 0 )
-            out.write( buffer, 0, bytesRead );
-
-        in.close();
-
-        response.sendRedirect("/databases/FileManager");
-    }
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        Integer id = Integer.valueOf(request.getParameter("id"));
+//        String path = getFilePath(id);
+//
+//        FileInputStream in = new FileInputStream(path);
+//        OutputStream out = response.getOutputStream();
+//
+//        byte buffer[] = new byte[2048];
+//        int bytesRead;
+//        while( (bytesRead = in.read( buffer )) > 0 )
+//            out.write( buffer, 0, bytesRead );
+//
+//        in.close();
+//
+//        response.sendRedirect("/databases/FileManager");
+//     
+//    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	doGet(request, response);
     }
 
 }
-
