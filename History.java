3f5c2f3af155c0191a4e0fc68ad4d2f7581c685d
ajetty <22653345+ajetty@users.noreply.github.com>
2018-08-08 04:47:43 -0700

    Add files via upload

---
+++ History.java
@@ -0,0 +1,70 @@
+package dbs;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = "/databases/History", loadOnStartup = 1)
+public class History extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    Connection c = null;
+	    Integer id = Integer.valueOf(request.getParameter("id"));
+	    List<Upload> files = new ArrayList<Upload>();
+	    
+	    try
+	    {
+	        String url = "jdbc:mysql://cs3.calstatela.edu:3306/cs3220stu12?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	        String username = "cs3220stu12";
+	        String password = "EOZ6mrUV";
+	
+	        String sql = "SELECT * FROM history WHERE record_id =" + id;
+	
+	        c = DriverManager.getConnection(url, username, password);
+	        PreparedStatement pstmt = c.prepareStatement(sql);
+	        ResultSet rs = pstmt.executeQuery();
+	
+	        while(rs.next()) {
+                Upload file = new Upload(Integer.parseInt(rs.getString("record_id")), rs.getString("fileName"), rs.getString("filePath"), rs.getString("date"));
+                files.add(file);
+	        }
+	    }
+	    catch(SQLException e)
+	    {
+	        throw new ServletException(e);
+	    }
+	    finally
+	    {
+	        try
+	        {
+	            if(c != null) c.close();
+	        }
+	        catch(SQLException e)
+	        {
+	            throw new ServletException(e);
+	        }
+	    }
+	    
+	    request.setAttribute("files", files);
+	    request.getRequestDispatcher("/WEB-INF/History.jsp").forward(request, response );
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ History.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>History</title>
+</head>
+<body>
+
+<c:choose>
+	<c:when test="${files.size() > 1}">
+	
+		<table border="1" style="border-collapse: collapse;text-align: left;">
+		<tr>
+			<th>ID</th>
+			<th>File Name</th>
+			<th>File Path</th>
+			<th>Options</th>
+		</tr>
+		
+		<c:forEach items="${files}" var="file">
+		<tr>
+			<td>${file.id}</td>
+			<td>${file.fileName}</td>
+			<td>${file.filePath}</td>
+			<td>
+				<a href="Delete?id=${file.id}">Delete</a>
+				<a href="Download?id=${file.id}">Download</a> 
+				<a href="Update?id=${file.id}">Update</a>
+			</td>
+		</tr>
+		</c:forEach>
+		</table>
+		<a href="FileManager">Go back to Main.</a>
+	</c:when>
+	<c:otherwise>
+		<h3>There is no history for this file!</h3>
+		<a href="FileManager">Go back to Main.</a>
+	</c:otherwise>
+</c:choose>
+</body>
+</html>
